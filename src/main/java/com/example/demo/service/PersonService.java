854d1ad76217d344c1ec9e39d7691b55f745c89f
ktguy0723 <ktguy.yy@gmail.com>
2021-08-30 19:30:17 +0900

    post実行可能

---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+// @Componentとおなじ
+@Service
+public class PersonService {
+  
+  private final PersonDao personDao;
+
+  // @Autowired: コンストラクタインジェクションを使用するときにつける（4.3移行省略可）
+  // https://reasonable-code.com/spring-injection-method/
+  // @Qualifierは、フィールドにautowiredするBeanを意味します
+  // https://ja.getdocs.org/spring-spring-autowiring-qualifier-example/
+  @Autowired
+  public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+    this.personDao = personDao;
+  }
+  
+  public int addPerson(Person person) {
+    return personDao.insertPerson(person);
+  }
+}
